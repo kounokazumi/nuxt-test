001a3f0c5e4f6803cd6284f7b5739d5b151ddb5a
piokn0713 <kazumi0526kcsk@gmail.com>
2021-10-27 10:59:12 +0900

    nuxtでカウンタ

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="container">
+    <div>
+      <h1>カウンター</h1>
+<blockquote>
+      <h2>{{count}}</h2>
+</blockquote>
+      <button @click="addCount">アップ</button><p />
+      <button @click="subtractCount">ダウン</button><p />
+      <button @click="clearCount">クリア</button><p />
+    </div>
+<blockquote>
+Apr/3/2019<p />
+</blockquote>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    count () { return this.$store.state.counter.count }
+  },
+  methods: {
+    addCount (e) {
+      this.$store.commit('counter/add')
+    },
+    subtractCount (e) {
+      this.$store.commit('counter/subtract')
+    },
+    clearCount (e) {
+      this.$store.commit('counter/clear')
+    }
+  }
+}
+</script>
---
+++ store/counter.js
@@ -0,0 +1,16 @@
+export const state = () => ({
+    count: 0
+  })
+  
+  export const mutations = {
+    add (state) {
+      state.count += 1
+    },
+    subtract (state) {
+      state.count -= 1
+    },
+    clear (state) {
+      state.count = 0
+    }
+  }
+
