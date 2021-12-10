e17cf89ffad07321fd4b2498eee9badf32df77eb
piokn0713 <kazumi0526kcsk@gmail.com>
2021-12-10 12:01:06 +0900

    vueインストール

---
+++ plugins/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDw2CtQY4S6sfPbBPiMdKQ35TS0ne64og",
+  authDomain: "sotuken-test.firebaseapp.com",
+  projectId: "sotuken-test",
+  storageBucket: "sotuken-test.appspot.com",
+  messagingSenderId: "349598854436",
+  appId: "1:349598854436:web:1d1b5f82200975efd797b1",
+  measurementId: "G-0J6YB1EVW9"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
