910db7e8a67cb7476b55ac85a4d61be630ea61ba
hamigakitanuki <kakizaki0213@gmail.com>
2022-01-18 19:45:59 +0900

    一旦削除

---
+++ pages/list.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="bl_exam">
+    <!-- ナビバー -->
+    <Navbar/>
+
+    <div class="bl_exam_main">
+      <div class="bl_box">
+        <ul>
+          <li>
+            <input class="check" type="checkbox"  name="scales" checked>リスト
+            <button class="flatbutton" >
+              <i class="fas fa-trash"></i>
+            </button>
+          </li>
+          <li>
+            <input class="check" type="checkbox"  name="scales" checked>リスト
+            <button class="flatbutton">
+              <i class="fas fa-trash"></i>
+            </button>
+          </li>
+        </ul>
+      </div>
+
+      <form class="bl_box" action="">
+        <input class="form" type="text" name="number" placeholder="リスト名">
+        <button class="save" type="submit">追加</button>
+      </form>
+
+    </div>
+    
+
+  </div>
+
+</template>
+
+<script>
+  export default{
+    // コンポーネント呼び出し
+    components: {
+      Navbar: () => import('~/layouts/navbar')
+    },
+
+    data(){
+      return{
+        isActive: "1"
+      }
+    },
+    methods: {
+    isSelect: function (num) {
+      this.isActive = num;
+    }
+  }
+    
+}
+</script>
+
+<style>
+</style>
--- pages/top.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <section class="top mx_auto" >
-              <div class="top_btns mt_20 tx_center"><a class="top_btns_link" href="http://localhost:3000/login">ログイン</a></div>
-              <div class="top_btns mt_20 tx_center"><a class="top_btns_link" href="http://localhost:3000/new_acount">新規会員登録</a></div>
-  </section>
-</template>
