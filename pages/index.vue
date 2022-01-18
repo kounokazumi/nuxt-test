259a92ba7a0a56c8f4c4609be1d450bd0674b156
hamigakitanuki <kakizaki0213@gmail.com>
2022-01-18 14:16:47 +0900

    feat:top作成

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bl_top d_flex fl_dir_col al_center ju_between">
+    <img class="bl_top_logo" src="~/assets/img/logo.png" alt="">
+    <div class="d_grid ju_items_center gap_30">
+      <a class="el_btn bcol_orange col_white" href="">ログイン</a>
+      <a class="el_btn bcol_orange col_white" href="">新規会員登録</a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+.bl_top{
+  height: 100%;
+  &_logo{
+    width: 150px;
+  }
+}
+</style>
