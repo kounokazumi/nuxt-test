308ea8807d607fd6afdce0e31672aa44456c4cce
hamigakitanuki <kakizaki0213@gmail.com>
2022-01-18 19:49:06 +0900

    一旦削除

--- layouts/navbar.vue
+++ layouts/navbar.vue
@@ -1,8 +1,11 @@
 <template>
-<nav class="ly_navbar">
-    <ul class="ly_navbar_tabList">
-        <li class="ly_navbar_tabList_item" v-on:click="isSelect('1')">買い物リスト</li>
-        <li class="ly_navbar_tabList_item" v-on:click="isSelect('2')">在庫リスト</li>
-    </ul>
-</nav>
+  <nav class="ly_navbar">
+      <ul class="ly_navbar_tabList">
+          <li class="ly_navbar_tabList_item" v-on:click="isSelect('1')">買い物リスト</li>
+          <li class="ly_navbar_tabList_item" v-on:click="isSelect('2')">在庫リスト</li>
+      </ul>
+  </nav>
 </template>
+<style lang="scss" scoped>
+
+</style>
