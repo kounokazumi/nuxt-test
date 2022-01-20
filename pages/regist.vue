c93f31f69f8838fa0fdf4d2f713b045d92578372
hamigakitanuki <kakizaki0213@gmail.com>
2022-01-20 22:42:06 +0900

    feat:会員登録まで

--- pages/regist.vue
+++ pages/regist.vue
@@ -2,14 +2,53 @@
   <div>
     <h2 class="fs_30 tx_center">新規会員登録</h2>
     <form class="mt_100 d_grid gap_30" action="">
-      <input class="el_input" type="text" name="" id="" placeholder="名前">
-      <input class="el_input" type="text" name="" id="" placeholder="メールアドレス">
-      <input class="el_input" type="text" name="" id="" placeholder="パスワード">
-      <button class="el_btn bcol_orange col_white w_100 fs_12">登録</button>
+      <input class="el_input" v-model="name" type="text" placeholder="名前" autocomplete="username">
+      <input class="el_input" v-model="email" type="email" placeholder="メールアドレス" autocomplete="email">
+      <input class="el_input" v-model="password" type="password" placeholder="パスワード" autocomplete="new-password">
+      <button class="el_btn bcol_orange col_white w_100 fs_12" type="button" v-on:click="regist">登録</button>
     </form>
   </div>  
 </template>
 
-<style lang="scss" scoped>
+<script>
+import { getAuth, createUserWithEmailAndPassword,onAuthStateChanged } from "firebase/auth";
+export default {
+  data(){
+    return {
+      name:'',
+      email:'',
+      password:'',
+    }
+  },
+  methods:{
+    regist(){
+      const auth = this.$fb.auth();
 
-</style> 
+      // ユーザーの作成
+      createUserWithEmailAndPassword(auth, this.email, this.password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          this.$router.push("/buylist");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode,errorMessage);
+        });
+    },s
+  },
+  mounted(){
+    const auth = this.$fb.auth();
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // ログイン済み
+        this.$router.push("/buylist");
+      } else {
+        // 未ログイン
+      }
+    });
+  }
+}
+</script>
