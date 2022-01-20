54e0cb5efcc8d1912faa0dfa59f6da1aa8b90965
hamigakitanuki <kakizaki0213@gmail.com>
2022-01-20 21:49:25 +0900

    ログインと会員登録

---
+++ pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2 class="fs_30 tx_center">ログイン</h2>
+    <form class="mt_100 d_grid gap_30" action="">
+      <input class="el_input" type="text" name="" id="" placeholder="メールアドレス">
+      <input class="el_input" type="text" name="" id="" placeholder="パスワード">
+      <button class="el_btn bcol_orange col_white w_100 fs_12">ログイン</button>
+      <a class="col_orange w_100 tx_center" href="">パスワードをお忘れの方はこちら</a>
+    </form>
+  </div>  
+</template>
+
+<style lang="scss" scoped>
+
+</style> 
---
+++ pages/regist.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2 class="fs_30 tx_center">新規会員登録</h2>
+    <form class="mt_100 d_grid gap_30" action="">
+      <input class="el_input" type="text" name="" id="" placeholder="名前">
+      <input class="el_input" type="text" name="" id="" placeholder="メールアドレス">
+      <input class="el_input" type="text" name="" id="" placeholder="パスワード">
+      <button class="el_btn bcol_orange col_white w_100 fs_12">登録</button>
+    </form>
+  </div>  
+</template>
+
+<style lang="scss" scoped>
+
+</style> 
