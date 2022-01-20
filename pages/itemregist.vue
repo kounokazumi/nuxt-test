9436ee300fb72715df49ae74fc6f5df3163def40
hamigakitanuki <kakizaki0213@gmail.com>
2022-01-20 22:03:41 +0900

    買うものと在庫登録

---
+++ pages/itemregist.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2 class="fs_30 tx_center">買うものを追加</h2>
+    <form class="mt_100 d_grid gap_30" action="">
+      <input class="el_input" type="text" name="" id="" placeholder="名前">
+      <button class="el_btn bcol_orange col_white w_100 fs_12">登録</button>
+    </form>
+  </div>  
+</template>
+
+<style lang="scss" scoped>
+
+</style> 
---
+++ pages/stockregist.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2 class="fs_30 tx_center">在庫を追加</h2>
+    <form class="mt_100 d_grid gap_30" action="">
+      <input class="el_input" type="text" name="" id="" placeholder="名前">
+      <input class="el_input" type="date" name="" id="" placeholder="いつまで">
+      <button class="el_btn bcol_orange col_white w_100 fs_12">登録</button>
+    </form>
+  </div>  
+</template>
+
+<style lang="scss" scoped>
+
+</style> 
