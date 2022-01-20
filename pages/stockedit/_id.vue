fd6cddbdf4ce6e5bf41d0aacec3610a47947986b
hamigakitanuki <kakizaki0213@gmail.com>
2022-01-20 22:06:43 +0900

    買うものと在庫の編集画面

---
+++ pages/stockedit/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2 class="fs_30 tx_center">在庫を編集</h2>
+    <form class="mt_100 d_grid gap_30" action="">
+      <input class="el_input" type="text" name="" id="" placeholder="名前">
+      <input class="el_input" type="date" name="" id="" placeholder="いつまで">
+      <button class="el_btn bcol_orange col_white w_100 fs_12">登録</button>
+      <button class="el_btn bcol_red col_white w_100 fs_12">削除</button>
+    </form>
+  </div>  
+</template>
+
+<style lang="scss" scoped>
+
+</style> 
