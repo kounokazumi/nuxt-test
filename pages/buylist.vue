0fc466e97e5ac5c54f768d6159809c9134e00e5b
hamigakitanuki <kakizaki0213@gmail.com>
2022-01-20 22:00:43 +0900

    在庫と買い物リスト

---
+++ pages/buylist.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bl_itemManage">
+    <Navbar/>
+
+    <ul class="bl_itemManage_list d_grid gap_25 mt_30">
+      <li class="bl_itemManage_list_item d_grid al_center gap_15">
+        <input class="bl_itemManage_list_item_check" type="checkbox" name="" id="">
+        <p class="bl_itemManage_list_item_name">アイテム名が入ります</p>
+        <div class="bl_itemManage_list_item_icons d_grid gap_5">
+          <i class="bl_itemManage_list_item_icons_success fas fa-circle"></i>
+          <i class="bl_itemManage_list_item_icons_error fas fa-minus-circle"></i>
+        </div>
+      </li>
+    </ul>
+    
+    <button class="bl_itemManage_addBtn d_flex al_center ju_center">
+      <i class="bl_itemManage_addBtn_icon fas fa-plus-circle"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+  export default{
+    // コンポーネント呼び出し
+    components: {
+      Navbar: () => import('~/layouts/navbar')
+    },
+    
+}
+</script>
---
+++ pages/stocklist.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bl_itemManage">
+    <Navbar/>
+
+    <ul class="bl_itemManage_list d_grid gap_25 mt_30">
+      <li class="bl_itemManage_list_item d_grid al_center gap_15 p_rel">
+        <input class="bl_itemManage_list_item_check" type="checkbox" name="" id="">
+        <span class="bl_itemManage_list_item_status col_theme">あと3日</span>
+        <p class="bl_itemManage_list_item_name">アイテム名が入ります</p>
+        <div class="bl_itemManage_list_item_icons d_grid gap_5">
+          <i class="bl_itemManage_list_item_icons_success fas fa-plus-circle"></i>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default{
+    // コンポーネント呼び出し
+    components: {
+      Navbar: () => import('~/layouts/navbar')
+    },
+    
+}
+</script>
+
